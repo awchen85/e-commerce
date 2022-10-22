3fc8e472c9bfb718c1c4dcac07c5c3466f5f148d
awchen <awchen85@gmail.com>
2022-10-22 11:07:17 -0400

    created models

---
+++ models/Category.js
@@ -0,0 +1,33 @@
+const { INTEGER } = require('sequelize');
+const { STRING } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Category extends Model {}
+
+Category.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+  },
+  {
+    category_name: {
+      DataTypes: STRING,
+      allowNull: false
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
+);
+
+module.exports = Category;
